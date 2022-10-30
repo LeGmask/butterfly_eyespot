d1236c93f60b6ee609b3716525db3c3967865e5a
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-10-30 23:12:04 +0100

    [webapp:components] Add Button component

---
+++ webapp/src/lib/Button/Button.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let className = '';
+    export let color = '';
+    export let hover = false;
+    export let button;
+
+    if ($$slots.icon) {
+        className += ' btn-grid ';
+    }
+
+    if (color) {
+        className += ` btn-${color}`;
+    }
+
+    if (hover) {
+        className += ` btn-hover`;
+    }
+</script>
+
+<button bind:this={button} on:click class={className.trim()} {...$$restProps}>
+    {#if $$slots.icon}
+        <span class="icon"><slot name="icon"/></span>
+    {/if}
+    <span><slot/></span>
+</button>
+
+<style lang="scss">
+  @import './Button';
+</style>
