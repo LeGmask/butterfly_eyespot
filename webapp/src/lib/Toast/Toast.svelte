b48a9fba623c8bf4757fb9851da0aaf60b4b7761
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-10-30 23:11:48 +0100

    [webapp:components] Add Toaster component

---
+++ webapp/src/lib/Toast/Toast.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import {Icon} from "@steeze-ui/svelte-icon";
+	import {X} from "@steeze-ui/heroicons";
+
+	export let dismissable = true;
+	export let color = '';
+	let closed = false;
+
+	let onDismiss = () => {
+		closed = true;
+	};
+</script>
+
+<div class="toast {color}" class:closed class:dismissable>
+	<slot />
+	{#if dismissable}
+		<span class="dismiss" on:click={onDismiss}>
+			<Icon src="{X}" slot="icon" />
+		</span>
+	{/if}
+</div>
+
+<style lang="scss">
+	@import 'Toast';
+</style>
